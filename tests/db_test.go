d5c3570a9901325daf24e7d44149640848d00c2d
gogazub <gogazub101@gmail.com>
2025-10-24 23:55:32 +0300

    test: declare db tests

--- tests/db_test.go
+++ tests/db_test.go
@@ -1,1 +1,52 @@
 package tests
+
+import (
+	"testing"
+)
+
+type mockRepo struct {
+}
+
+func newMockRepo(t *testing.T) *mockRepo {
+	return &mockRepo{}
+}
+
+func makeOrder() interface{} {
+	return nil
+}
+
+// ---------- Save ----------
+
+func TestDBRepository_Save(t *testing.T) {
+	t.Run("success: upsert всех сущностей в одной транзакции -> commit", func(t *testing.T) {
+		// TODO
+	})
+
+	t.Run("error: orders upsert падает -> rollback и ошибка наружу", func(t *testing.T) {
+		// TODO
+	})
+}
+
+// ---------- GetByID ----------
+
+func TestDBRepository_GetByID(t *testing.T) {
+	t.Run("success: грузим order + delivery + payment + items", func(t *testing.T) {
+		// TODO
+	})
+
+	t.Run("not found: orders возвращает sql.ErrNoRows", func(t *testing.T) {
+		// TODO
+	})
+}
+
+// ---------- GetAll ----------
+
+func TestDBRepository_GetAll(t *testing.T) {
+	t.Run("success: читаем список id и для каждого вызываем GetByID", func(t *testing.T) {
+		// TODO
+	})
+
+	t.Run("error: SELECT order_uid падает", func(t *testing.T) {
+		// TODO
+	})
+}
